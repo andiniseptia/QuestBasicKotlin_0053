b8ab918e2379151d277948fd37340ba5768e3bac
andiniseptia <andiniseptiandri124@gmail.com>
2024-10-12 23:33:55 +0700

    strLength memastikan keamanan saat memanggil nilainya.

--- app/src/main/java/com/example/pam_project1/NullSafety.kt
+++ app/src/main/java/com/example/pam_project1/NullSafety.kt
@@ -21,4 +21,14 @@ fun nullSafety() {
         println("inferredNonNull is not null")
     }
 
+    // Safe call operator
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+
+    // notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) // 18
 }
