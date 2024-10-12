da9abb52af87051ae4f8fa14f939b09fed6a4c43
andiniseptia <andiniseptiandri124@gmail.com>
2024-10-12 22:27:09 +0700

    Menggunakan If-Else untuk memeriksa apakah nilai number lebih dari 0 atau tidak, jika iya akan menampilkan "Positive Number", jika tidak akan menampilkan "Negative Number",

---
+++ app/src/main/java/com/example/pam_project1/ConditionalExpression.kt
@@ -0,0 +1,17 @@
+package com.example.pam_project1
+
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    // If-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    }
+    else {
+        println("Negative Number")
+    }
+}
