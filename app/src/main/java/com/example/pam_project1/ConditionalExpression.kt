42e643dd744682bb8185f9f93fa3e6bfae883a1e
andiniseptia <andiniseptiandri124@gmail.com>
2024-10-12 22:49:03 +0700

    Mendeklarasikan daftar mobil dengan listOf, menggunakan for untuk melakukan perulangan dalam daftar cars. Pada setiap perulangan, variable car akan mengabil nilai dari data.

--- app/src/main/java/com/example/pam_project1/ConditionalExpression.kt
+++ app/src/main/java/com/example/pam_project1/ConditionalExpression.kt
@@ -46,4 +46,9 @@ fun Perulangan() {
     for (i in 1..5) {
         println("Perulangan ke-$i")
     }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
 }
