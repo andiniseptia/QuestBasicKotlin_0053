5a03202b6f7cf5dd8942b67671118075f9ee0fb3
andiniseptia <andiniseptiandri124@gmail.com>
2024-10-12 23:08:16 +0700

    membuat data class bernama DataClass dengan properti id dengan tipe data int dan email dengan tipe data string.

---
+++ app/src/main/java/com/example/pam_project1/DataClass.kt
@@ -0,0 +1,11 @@
+package com.example.pam_project1
+
+// Data Class
+// Data class adalah class yang digunakan untuk menyimpan data.
+// Data class menyediakan fungsi untuk meng-override fungsi equals(), hashCode(), dan toString().
+// Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
