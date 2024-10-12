ee5f881e0ae33300c1202f61ca4378f74791960a
andiniseptia <andiniseptiandri124@gmail.com>
2024-10-12 23:28:58 +0700

    neverNull dideklarasi sebagai string yang tidak bisa bernilai null.

---
+++ app/src/main/java/com/example/pam_project1/NullSafety.kt
@@ -0,0 +1,9 @@
+package com.example.pam_project1
+
+fun nullSafety() {
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    // Throws a compiler error
+    // neverNull = null
+}
