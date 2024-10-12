6ac062b24628e726ecc39640960fc3b1de5f9478
andiniseptia <andiniseptiandri124@gmail.com>
2024-10-12 21:49:23 +0700

    Menambah data yang keynya "Retangle" dan valuenya "4", serta menghapus pasangan key-value dari mutable map dengan key "Circle" dari mutable map shape yang sudah dibuat sebelumnya.

--- app/src/main/java/com/example/pam_project1/Collections.kt
+++ app/src/main/java/com/example/pam_project1/Collections.kt
@@ -82,6 +82,13 @@ fun contohMap() {
     val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(shape)
 
+    // Menambahkan data ke dalam Map Mutable
+    shape["Retangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
 }
 
 fun main() {
